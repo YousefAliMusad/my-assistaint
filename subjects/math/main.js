d149df62fd75547e33d9077a4a0427dfeec8298b
Yousaf <musadali996@gmail.com>
2023-02-25 21:12:14 +0200

    version 2

--- subjects/math/main.js
+++ subjects/math/main.js
@@ -3,6 +3,10 @@ let ul = document.querySelector("ul");
 let items = document.querySelector(".items");
 let add = document.querySelector(".button");
 let Delete = document.querySelector(".delete");
+let confirmZone = document.querySelector(".fa-confirm")
+let confirm = document.querySelector(".fa-confirm")
+let accept = document.querySelector(".agree")
+let refuse = document.querySelector(".refuse")
 var vals = JSON.parse(localStorage.getItem("item3")) ? JSON.parse(localStorage.getItem("item3")) : [];
 const data = JSON.parse(localStorage.getItem("item3")) ? JSON.parse(localStorage.getItem("item3")) : [];
 
@@ -11,7 +15,7 @@ const data_rank = JSON.parse(localStorage.getItem("item-rank3")) ? JSON.parse(lo
 
 
 function creteLi(val , Valrank) {
-    let item = document.createElement("div");
+    var item = document.createElement("div");
     item.className = "item"
     let cont = document.createElement("div");
     cont.className = "cont"
@@ -31,20 +35,32 @@ data.map(function(item , rVal){
 })
 
 add.onclick = function(){
-    for(let i = 0; i <= vals.length ; i++) {
-        var rank = i
+    if (inp.value != "") {
+        for(let i = 0; i <= vals.length ; i++) {
+            var rank = i
+        }
+        itam_rank.push(rank)
+        itam_rank.push(rank)
+        vals.push(inp.value)
+        vals.push(inp.value)
+        localStorage.setItem("item", JSON.stringify(vals))
+        creteLi(inp.value , rank)
+        inp.value = ""
+        // console.log(vals.includes("غايب"))
     }
-    itam_rank.push(rank)
-    vals.push(inp.value)
-    localStorage.setItem("item3", JSON.stringify(vals))
-    creteLi(inp.value , rank)
-    inp.value = ""
 }
 
 
 
 
 Delete.onclick = function(){
+    confirmZone.style.cssText = "display:flex"
+}
+refuse.onclick = function() {
+    confirmZone.style.cssText = "dispaly:none"
+}
+accept.onclick = function() {
+    confirmZone.style.cssText = "display:none"
     localStorage.removeItem("item3")
     localStorage.removeItem("item-rank3")
     items.innerHTML = ""
--- subjects/social-studies/main.js
+++ subjects/social-studies/main.js
@@ -1,8 +1,12 @@
 let inp = document.querySelector(".inp");
 let ul = document.querySelector("ul");
-let items1 = document.querySelector(".items");
+let items = document.querySelector(".items");
 let add = document.querySelector(".button");
-let Delete1 = document.querySelector(".delete");
+let Delete = document.querySelector(".delete");
+let confirmZone = document.querySelector(".fa-confirm")
+let confirm = document.querySelector(".fa-confirm")
+let accept = document.querySelector(".agree")
+let refuse = document.querySelector(".refuse")
 var vals = JSON.parse(localStorage.getItem("item5")) ? JSON.parse(localStorage.getItem("item5")) : [];
 const data = JSON.parse(localStorage.getItem("item5")) ? JSON.parse(localStorage.getItem("item5")) : [];
 
@@ -11,7 +15,7 @@ const data_rank = JSON.parse(localStorage.getItem("item-rank5")) ? JSON.parse(lo
 
 
 function creteLi(val , Valrank) {
-    let item = document.createElement("div");
+    var item = document.createElement("div");
     item.className = "item"
     let cont = document.createElement("div");
     cont.className = "cont"
@@ -24,29 +28,41 @@ function creteLi(val , Valrank) {
     date.className = "date"
     cont.appendChild(grade);
     cont.appendChild(date);
-    items1.appendChild(item);
+    items.appendChild(item);
 }
 data.map(function(item , rVal){
     creteLi(item , rVal)
 })
 
 add.onclick = function(){
-    for(let i = 0; i <= vals.length ; i++) {
-        var rank = i
+    if (inp.value != "") {
+        for(let i = 0; i <= vals.length ; i++) {
+            var rank = i
+        }
+        itam_rank.push(rank)
+        itam_rank.push(rank)
+        vals.push(inp.value)
+        vals.push(inp.value)
+        localStorage.setItem("item", JSON.stringify(vals))
+        creteLi(inp.value , rank)
+        inp.value = ""
+        // console.log(vals.includes("غايب"))
     }
-    itam_rank.push(rank)
-    vals.push(inp.value)
-    localStorage.setItem("item5", JSON.stringify(vals))
-    creteLi(inp.value , rank)
-    inp.value = ""
 }
 
 
 
 
-Delete1.onclick = function(){
+Delete.onclick = function(){
+    confirmZone.style.cssText = "display:flex"
+}
+refuse.onclick = function() {
+    confirmZone.style.cssText = "dispaly:none"
+}
+accept.onclick = function() {
+    confirmZone.style.cssText = "display:none"
     localStorage.removeItem("item5")
     localStorage.removeItem("item-rank5")
-    items1.innerHTML = ""
+    items.innerHTML = ""
     vals = []
 }
